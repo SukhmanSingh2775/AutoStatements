337452b2ab8a33ca2d3bf08473d13e4764d3f16b
SukhmanSingh2775 <63064932+SukhmanSingh2775@users.noreply.github.com>
2020-10-10 19:42:11 -0700

    Add files via upload
    Final Version

--- RelayStatements/statements.py
+++ RelayStatements/statements.py
@@ -5,6 +5,10 @@ from selenium import webdriver
 from docx.enum.text import WD_ALIGN_PARAGRAPH 
 import time
 
+import xlrd
+import openpyxl
+
+
 from docx import Document
 from docx.shared import RGBColor
 from docx.shared import Pt
@@ -24,7 +28,7 @@ signInBtn.click()
 gold_star_username = 'ranjitsingh9774@gmail.com'
 akal_trucking_username = 'harman4985@gmail.com'
 
-gold_star_password = "Simar1998"
+gold_star_password = "Singh20"
 akal_trucking_password = "Amazon4980"
 
 account = int(input("Which Account? Goldstar (1) , Akal (2)"))
@@ -63,7 +67,7 @@ browser.get('https://relay.amazon.com/tours/tours?state=history')
 time.sleep(1)
 
     
-
+final_load_id = []
 
 def getStatements():
     
@@ -184,6 +188,7 @@ def getStatements():
             
     
     final_prices = [s.replace(',','') for s in final_prices]
+    final_prices = [s for s in final_prices if s]
 
     final_load_id = [s.replace('.','') for s in final_load_id]
     final_load_id = [s[1:] if len(s) > 4 else s for s in final_load_id]
@@ -204,9 +209,8 @@ def getStatements():
 
     x = 0
     a = 0
-
-
-
+    
+    final_cities = [s for s in final_cities if s]
     for i in range(len(final_cities) // 2):
 
             load_paragraph = document.add_paragraph(style="List Number")
@@ -263,8 +267,6 @@ def getStatements():
         
         
             document.add_paragraph('')
-
-
 
 
 
@@ -311,197 +313,232 @@ for i in range(0, number_of_statements):
     
     
     
-    
-    
-    
-    
-    
-    
-    
-time.sleep(2)
+# path = ("/Users/sukhmansingh/Downloads/ExcelStatement1.xlsx")
+
+# wb = xlrd.open_workbook(path)
+# sheet = wb.sheet_by_index(1)
+
+# load_list_1 = sheet.col_values(1) #Unmodified round trip loads
+# load_list_2 = sheet.col_values(2) #Unmodified one way laods
+# prices_column = sheet.col_values(21) # Unmodified prices
+
+# round_trips = [i[-4:] for i in load_list_1  if i != "Trip ID"] # Round Trips Loads
+# load_list_2 = [i[-4:] for i in load_list_2 if i != "Load ID"] # One Way Loads
+# prices_column = [i for i in prices_column if type(i) != str] # Getting float only for prices
+# prices_column.pop(-1)
+
+# one_way = [load_list_2[i] for i in range(len(round_trips)) if round_trips[i] == "" or round_trips[i] == "-" if load_list_2[i] != ""] #Modified one way loads
+
+# prices_one_way = [prices_column[i] for i in range(len(prices_column)) if load_list_1[i] == "" or load_list_1[i] == "-"]
+# prices_round_trip = [prices_column[i] for i in range(len(prices_column)) if load_list_1[i] and load_list_1[i] != "-"]
+# print("Round trips are " + str(round_trips))
+# print("One way loads are " + str(one_way))
+# print("Final load id is " + str(final_load_id))
+            
+# for i in range(len(final_load_id)):
+#     if final_load_id[i] in one_way or final_load_id[i] in round_trips:
+#         if (final_load_id[i] in one_way):
+#             print("Found it in one way")
+#         elif (final_load_id[i] in round_trips):
+#             print("Found it in round trips")
 
 
-def getCancelledLoads():
-    
-    document = Document()
-    search_bar = browser.find_element_by_xpath('/html/body/div[22]/main/div[5]/div/div/div[1]/div/div[1]/input')
-    search_bar.click()
-    time.sleep(1)
-    clear = browser.find_element_by_xpath('//*[@id="application"]/div/div/div[1]/div/div[1]/i')
-    clear.click()
-    time.sleep(2)
-    
-    filter_btn = browser.find_element_by_xpath('//*[@id="application"]/div/div/div[1]/div/div[2]/div[2]/span/span')
-    
-    filter_btn.click()
-    
-    cancelled_btn = browser.find_element_by_xpath('//*[@id="application"]/div/div/div[1]/div/div[2]/div[2]/div/div[9]/span')
-    cancelled_btn.click()
-    
-    
-    time.sleep(2)
-    
-    cities = browser.find_elements_by_class_name("city")
-    
-    prices = browser.find_elements_by_class_name('tour-header__payout--normal--primary')
-    loadId = browser.find_elements_by_class_name('tour-header__tour-id__sliced')
-    times = browser.find_elements_by_class_name('tour-header__secondary ')
-    dateRange = browser.find_element_by_xpath('//*[@id="application"]/div/div/span/div/div/span').text
 
 
-    final_cities = []
-    final_prices = []
-    final_load_id = []
-    final_times = []
 
 
-    dateRangeHeading = document.add_heading('',2)
-    run = dateRangeHeading.add_run(dateRange)
-    font = run.font
-    font.color.rgb = RGBColor(170, 0, 255)
 
-    dateRangeHeading.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+
+
+    
+    
+    
+    
+
+
+
+# def getCancelledLoads():
+    
+#     document = Document()
+#     search_bar = browser.find_element_by_xpath('/html/body/div[22]/main/div[5]/div/div/div[1]/div/div[1]/input')
+#     search_bar.click()
+#     time.sleep(1)
+#     clear = browser.find_element_by_xpath('//*[@id="application"]/div/div/div[1]/div/div[1]/i')
+#     clear.click()
+#     time.sleep(2)
+    
+#     filter_btn = browser.find_element_by_xpath('//*[@id="application"]/div/div/div[1]/div/div[2]/div[2]/span/span')
+    
+#     filter_btn.click()
+    
+#     cancelled_btn = browser.find_element_by_xpath('//*[@id="application"]/div/div/div[1]/div/div[2]/div[2]/div/div[9]/span')
+#     cancelled_btn.click()
+    
+    
+#     time.sleep(2)
+    
+#     cities = browser.find_elements_by_class_name("city")
+    
+#     prices = browser.find_elements_by_class_name('tour-header__payout--normal--primary')
+#     loadId = browser.find_elements_by_class_name('tour-header__tour-id__sliced')
+#     times = browser.find_elements_by_class_name('tour-header__secondary ')
+#     dateRange = browser.find_element_by_xpath('//*[@id="application"]/div/div/span/div/div/span').text
+
+
+#     final_cities = []
+#     final_prices = []
+#     final_load_id = []
+#     final_times = []
+
+
+#     dateRangeHeading = document.add_heading('',2)
+#     run = dateRangeHeading.add_run(dateRange)
+#     font = run.font
+#     font.color.rgb = RGBColor(170, 0, 255)
+
+#     dateRangeHeading.alignment = WD_ALIGN_PARAGRAPH.CENTER
     
    
     
-    companyName = ''
+#     companyName = ''
     
-    if (username == gold_star_username):
-        companyName = 'Goldstar Trucking'
-        companyNameHeading = document.add_heading('',2)
-        run = companyNameHeading.add_run(companyName)
-        font = run.font
-        font.color.rgb = RGBColor(170, 0, 255)
+#     if (username == gold_star_username):
+#         companyName = 'Goldstar Trucking'
+#         companyNameHeading = document.add_heading('',2)S
+#         run = companyNameHeading.add_run(companyName)
+#         font = run.font
+#         font.color.rgb = RGBColor(170, 0, 255)
     
     
-    else:
-        companyName = 'Akal Trucking'
-        companyNameHeading = document.add_heading('',2)
-        run = companyNameHeading.add_run(companyName)
-        font = run.font
-        font.color.rgb = RGBColor(170, 0, 255)
+#     else:
+#         companyName = 'Akal Trucking'
+#         companyNameHeading = document.add_heading('',2)
+#         run = companyNameHeading.add_run(companyName)
+#         font = run.font
+#         font.color.rgb = RGBColor(170, 0, 255)
 
 
-    document.add_heading('',2)
+#     document.add_heading('',2)
 
-    for i in range(0, len(cities)):
-        try:
-            final_cities.append(cities[i].text)
+#     for i in range(0, len(cities)):
+#         try:
+#             final_cities.append(cities[i].text)
         
-            final_prices.append(prices[i].text)
+#             final_prices.append(prices[i].text)
             
-            final_load_id.append(loadId[i].text)
+#             final_load_id.append(loadId[i].text)
 
         
             
             
-        except:
-            pass
+#         except:
+#             pass
             
 
 
 
     
-    final_prices = [s.replace(',','') for s in final_prices]
+#     final_prices = [s.replace(',','') for s in final_prices]
 
-    final_load_id = [s.replace('.','') for s in final_load_id]
+#     final_load_id = [s.replace('.','') for s in final_load_id]
     
-    final_load_id = [s[1:] if len(s) > 4 else s for s in final_load_id]
+#     final_load_id = [s[1:] if len(s) > 4 else s for s in final_load_id]
     
         
-    try:
-        final_times = [times[i].text for i in range(len(times))]
-        final_times = [i[4:] for i in final_times if i]
-        final_times = [i[:-4] for i in final_times if i]
+#     try:
+#         final_times = [times[i].text for i in range(len(times))]
+#         final_times = [i[4:] for i in final_times if i]
+#         final_times = [i[:-4] for i in final_times if i]
     
-        final_times_list = [i for i in final_times if len(i) >= 10]
+#         final_times_list = [i for i in final_times if len(i) >= 10]
 
-    except:
-        pass
+#     except:
+#         pass
 
 
 
-    x = 0
+#     x = 0
     
     
 
-    for i in range(len(final_cities) // 2):
+#     for i in range(len(final_cities) // 2):
 
-            load_paragraph = document.add_paragraph(style="List Number")
+#             load_paragraph = document.add_paragraph(style="List Number")
         
-            try:
+#             try:
                 
-                run = load_paragraph.add_run("({})".format(final_times_list[x]) + "   " + "({})".format(final_times_list[x+1]) + " ")
-                font = run.font
-                font.color.rgb = RGBColor(170, 0, 255)
-                run.bold = True
+#                 run = load_paragraph.add_run("({})".format(final_times_list[x]) + "   " + "({})".format(final_times_list[x+1]) + " ")
+#                 font = run.font
+#                 font.color.rgb = RGBColor(170, 0, 255)
+#                 run.bold = True
 
                 
                 
 
-                run = load_paragraph.add_run(final_cities[x] + "   ")
-                font = run.font
-                font.color.rgb = RGBColor(33, 83, 163)
-                run.bold = True
+#                 run = load_paragraph.add_run(final_cities[x] + "   ")
+#                 font = run.font
+#                 font.color.rgb = RGBColor(33, 83, 163)
+#                 run.bold = True
                 
-                run = load_paragraph.add_run("To" + "   ")
-                font = run.font
-                font.color.rgb = RGBColor(10,20,30)
-                run.bold = True
-                
-                
-                run = load_paragraph.add_run(final_cities[x+1] + "   ")
-                font = run.font
-                font.color.rgb = RGBColor(33, 83, 163)
-                run.bold = True
+#                 run = load_paragraph.add_run("To" + "   ")
+#                 font = run.font
+#                 font.color.rgb = RGBColor(10,20,30)
+#                 run.bold = True
                 
                 
-                
-                run = load_paragraph.add_run(final_prices[i] + "   ")
-                font = run.font
-                font.color.rgb = RGBColor(247, 21, 5)
-                run.bold = True
+#                 run = load_paragraph.add_run(final_cities[x+1] + "   ")
+#                 font = run.font
+#                 font.color.rgb = RGBColor(33, 83, 163)
+#                 run.bold = True
                 
                 
                 
+#                 run = load_paragraph.add_run(final_prices[i] + "   ")
+#                 font = run.font
+#                 font.color.rgb = RGBColor(247, 21, 5)
+#                 run.bold = True
                 
-                run = load_paragraph.add_run("#" + final_load_id[i])
-                font = run.font
-                font.color.rgb = RGBColor(247, 5, 239)
-                run.bold = True
+                
+                
+                
+#                 run = load_paragraph.add_run("#" + final_load_id[i])
+#                 font = run.font
+#                 font.color.rgb = RGBColor(247, 5, 239)
+#                 run.bold = True
                 
             
                 
                 
-                x += 2
+#                 x += 2
             
-            except:
-                pass
+#             except:
+#                 pass
         
         
         
-            document.add_paragraph('')
+#             document.add_paragraph('')
 
 
 
 
 
-    lst = [e[1:] for e in final_prices]
+#     lst = [e[1:] for e in final_prices]
 
-    lst = [float(i) for i in lst]
+#     lst = [float(i) for i in lst]
 
-    total = sum(lst)
+#     total = sum(lst)
 
 
-    total_paragraph = document.add_paragraph()
-    run = total_paragraph.add_run("Total:" + "  " + "$" + str(int(total)))
-    font = run.font
-    run.bold = True
-    run.italic = True
-    run.underline = True
-    font.color.rgb = RGBColor(0, 255, 183)
-    total_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    font.size = Pt(24)
+#     total_paragraph = document.add_paragraph()
+#     run = total_paragraph.add_run("Total:" + "  " + "$" + str(int(total)))
+#     font = run.font
+#     run.bold = True
+#     run.italic = True
+#     run.underline = True
+#     font.color.rgb = RGBColor(0, 255, 183)
+#     total_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+#     font.size = Pt(24)
 
     
     
@@ -511,20 +548,20 @@ def getCancelledLoads():
     
     
     
-    path = '/Users/sukhmansingh/Desktop/Invoices2/'
+#     path = '/Users/sukhmansingh/Desktop/Invoices2/'
     
     
-    if (username == gold_star_username):
-        path = path + "Goldstar/"
-        document.save(path + '{}.docx'.format("CancelledLoads" +  ' ' + "{}".format(dateRange)))
+#     if (username == gold_star_username):
+#         path = path + "Goldstar/"
+#         document.save(path + '{}.docx'.format("CancelledLoads" +  ' ' + "{}".format(dateRange)))
   
     
-    else:
-        path = path + "Akal/"
-        document.save(path + '{}.docx'.format("CancelledLoads" +  ' ' + "{}".format(dateRange)))
+#     else:
+#         path = path + "Akal/"
+#         document.save(path + '{}.docx'.format("CancelledLoads" +  ' ' + "{}".format(dateRange)))
 
 
-run_cancel = input("Do you want to get the cancel statement? ")
+# run_cancel = input("Do you want to get the cancel statement? ")
 
-if run_cancel:
-    getCancelledLoads()
+# if run_cancel:
+#     getCancelledLoads()
